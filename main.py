ae3727251993328f6b6ee3ce689e1785ba622af4
phootoon <M.rozbicki1995@gmail.com>
2024-06-01 18:26:19 +0200

    fixed the length of the solution it finnally works a little bit

--- main.py
+++ main.py
@@ -1,63 +1,42 @@
 import functions
 
-l = 10
+# Parameters for the genetic algorithm
+POPULATION_SIZE = 100
+GENERATIONS = 1000
+MUTATION_RATE = 0.01
+DNA_WORD_LENGTH = 10
+TARGET_LENGTH = 206
 
-# Press the green button in the gutter to run the script.
+# Read and preprocess the data
 if __name__ == '__main__':
     file_path = 'data.txt'
     data = functions.openfile(file_path)
-    processeddata = functions.preproccesing(data,l)
-    print((processeddata)," - iloś elementów po precossenigu", len(data), " - ilość elementó początkowych")
-    listofindexestodelete=[]
+    processeddata = functions.preproccesing(data, DNA_WORD_LENGTH)
     processednorepetitions = functions.deletethesame(processeddata)
-    # print((processednorepetitions))
-    joined_words = functions.joinwords(processednorepetitions,data,l)
-    # print(joined_words)
+    joined_words = functions.joinwords(processednorepetitions, data, DNA_WORD_LENGTH)
 
-    # Define parameters for the genetic algorithm
-    pc_count = len(joined_words[0])
-    task_times = [1] * len(joined_words)  # Assuming each task takes equal time, modify as needed
-    starting_solution = [0] * len(joined_words)
-    pop_size = 100
-    max_generations = 1000
-    mutation_probability = 0.05
-    task_count = len(joined_words)
+    # Run the genetic algorithm
+    def genetic_algorithm():
+        population = functions.create_population(joined_words, POPULATION_SIZE, TARGET_LENGTH)
+        for generation in range(GENERATIONS):
+            parents = functions.select_parents(population, TARGET_LENGTH)
+            offspring = []
+            for i in range(0, len(parents), 2):
+                if i + 1 < len(parents):
+                    child1, child2 = functions.crossover(parents[i], parents[i + 1], TARGET_LENGTH)
+                    offspring.append(functions.mutate(child1, MUTATION_RATE, joined_words, TARGET_LENGTH))
+                    offspring.append(functions.mutate(child2, MUTATION_RATE, joined_words, TARGET_LENGTH))
+            population = parents + offspring
+            best_sequence = max(population, key=lambda x: functions.fitness(''.join(x), TARGET_LENGTH))
+            best_fitness = functions.fitness(''.join(best_sequence), TARGET_LENGTH)
+            print(f"Generation {generation}: Best Fitness = {best_fitness}")
+            if len(''.join(best_sequence)) == TARGET_LENGTH:
+                break
+        return best_sequence
 
-    # Solve the sequencing problem using the genetic algorithm
-    best_fitness, best_solution = functions.solve_pcmax(pc_count, task_times, starting_solution, pop_size, max_generations,
-                                              mutation_probability, task_count)
-    print("Best fitness:", best_fitness)
-    print("Best solution:", best_solution)
-    counter = 0
-    finalarray = []
-    for i in best_solution:
-        counter += len(joined_words[i])
-        finalarray.append(joined_words[i])
-
-    print(counter)
-    print(finalarray)
-
-
-
-
-
-    # pc_count = len(processednorepetitions)  # Number of unique DNA segments
-    # task_times = [len(data[i]) for i in
-    #               range(len(data))]  # Assuming each task time corresponds to the length of the segment
-    # starting_solution = [functions.random.randint(0, pc_count - 1) for _ in range(len(data))]
-    # pop_size = 50  # Population size for the genetic algorithm
-    # max_generations = 1000  # Maximum number of generations
-    # mutation_probability = 0.01  # Mutation probability
-    # task_count = len(data)  # Number of tasks
-    #
-    # best_fitness, best_solution = functions.solve_pcmax(pc_count, task_times, starting_solution, pop_size, max_generations,
-    #                                           mutation_probability, task_count)
-    #
-    # print(f"Best Fitness: {best_fitness}")
-    # print(f"Best Solution: {best_solution}")
-    #
-    # # Convert the best solution indices to actual sequences
-    # assembled_dna = [data[idx] for idx in best_solution]
-    #
-    # print(f"Assembled DNA Sequence: {''.join(assembled_dna)}")
-    #
+    # Get the best sequence
+    best_sequence = genetic_algorithm()
+    best_sequence_str = ''.join(best_sequence)
+    print("Best Sequence:", best_sequence_str)
+    print("Best Sequence Length:", len(best_sequence_str))
+    print("Indexes of words in best sequence:", [joined_words.index(word) for word in best_sequence])
